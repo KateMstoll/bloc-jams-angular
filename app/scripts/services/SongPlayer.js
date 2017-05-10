0a7a3832f2cd7e6effe87b346ebac4d0a1673aa6
Catherine Stoll <KateMstoll@gmail.com>
2017-05-09 18:12:48 -0700

    Checkpoint 10 Directives:Complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-     function SongPlayer(Fixtures) {
+     function SongPlayer($rootScope, Fixtures) {
           var SongPlayer = {};
  /**
  * @desc which album is playing
@@ -28,6 +28,13 @@
         formats: ['mp3'],
         preload: true
     });
+       
+       
+    currentBuzzObject.bind('timeupdate', function() {
+         $rootScope.$apply(function() {
+             SongPlayer.currentTime = currentBuzzObject.getTime();
+         });
+     });
  
       SongPlayer.currentSong = song;
    };
@@ -47,6 +54,13 @@
 * @type {Object}
 */
     SongPlayer.currentSong = null;
+    
+ 
+ /**
+ * @desc Current playback time (in seconds) of currently playing song
+ * @type {Number}
+ */
+    SongPlayer.currentTime = null;
                 
          
          
@@ -126,9 +140,9 @@
  * @param {Object} song
  */
     
-         SongPlayer.next = function() {
-     var currentSongIndex = getSongIndex(SongPlayer.currentSong);
-     currentSongIndex++;
+    SongPlayer.next = function() {
+        var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+        currentSongIndex++;
         
          if (currentSongIndex > currentAlbum.length) {
              stopSong();
@@ -138,11 +152,25 @@
          playSong(song);
      }
  };
+     
+/**
+ * @function setCurrentTime
+ * @desc Set current time (in seconds) of currently playing song
+ * @param {Number} time
+ */
+    
+         
+    SongPlayer.setCurrentTime = function(time) {
+       
+        if (currentBuzzObject) {
+            currentBuzzObject.setTime(time);
+        }
+    };     
          
           return SongPlayer;
      }
  
      angular
          .module('blocJams')
-         .factory('SongPlayer', ['Fixtures', SongPlayer]);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
